a4bfed6ca12ceea3d4db3428b0121a7fbcbf852a
KyleStoecker <98105706+KyleStoecker@users.noreply.github.com>
2022-08-22 22:29:18 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/* Hello.java
+
+* Student Name (Kyle Stoecker)
+* 8/22/2022
+* CMSC 255
+*/
+public class Hello{
+	public static void main(String[] args) {
+
+		System.out.println("<Your Name>");
+		System.out.println("<Java Program Name>");
+		System.out.println("<Program/Project Descprition>");
+		System.out.println("<Date Created>");
+		System.out.println("<Course Number and Section Number>");
+		System.out.println();
+	
+	}
+}
